dca477145c363d1a57c28b857bdc63e79c83fc10
Abhishek K <abhishekakade@gmail.com>
2019-02-09 20:24:49 +0530

    Updates

---
+++ precache-manifest.eb7ca12a98e80c5f7d8c76831500ad2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ebad8e7a9f5566e1598",
+    "url": "/todo-react/static/js/runtime~main.4ebad8e7.js"
+  },
+  {
+    "revision": "c5b3b085c0d2f5f7b3c5",
+    "url": "/todo-react/static/js/main.c5b3b085.chunk.js"
+  },
+  {
+    "revision": "c29f4fcce191ff77a782",
+    "url": "/todo-react/static/js/1.c29f4fcc.chunk.js"
+  },
+  {
+    "revision": "c5b3b085c0d2f5f7b3c5",
+    "url": "/todo-react/static/css/main.6ecd75c8.chunk.css"
+  },
+  {
+    "revision": "c29f4fcce191ff77a782",
+    "url": "/todo-react/static/css/1.a8cb0ab5.chunk.css"
+  },
+  {
+    "revision": "1f29d5ea5f0db196a9fe4c44435d9622",
+    "url": "/todo-react/index.html"
+  }
+];
